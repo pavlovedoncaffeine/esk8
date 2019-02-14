073ee2b556334eb21a77f62ae717d17ca150be2e
Aakash Ravichandran <akash.ravichandran@gmail.com>
2019-02-14 15:01:25 -0500

    Added Class connectedThread and two onClick function placeholders for LED on and LED off buttons
    <title>

--- app/src/main/java/com/a6ravich/esk8throttle/MainActivity.java
+++ app/src/main/java/com/a6ravich/esk8throttle/MainActivity.java
@@ -2,30 +2,49 @@ package com.a6ravich.esk8throttle;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.*;
 import android.bluetooth.*;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Set;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int ENABLE_BT_REQUEST_CODE = 1337;
+    public static Handler mHandler;
+
     private static final String TAG = "MainActivity";
 
     BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
     BluetoothDevice esk8 = null;
     Set<BluetoothDevice> pairedDev;
     BluetoothSocket btsocket;
+    private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
+    private String btStatus = null;
+
+    private Button LEDon;
+    private Button LEDoff;
+    private TextView mBTStatus;
+    private TextView mReadBuffer;
 
     //You can use a random UUID generator from the internet, or use the UUID.randomUUID() method
     //private final static UUID uuid = UUID.fromString("350320f0-b302-4f69-b827-53f3a203c3b2");
 
+    public static final int ENABLE_BT_REQUEST_CODE = 1337;
+    public final static int MESSAGE_READ = 2; // used in bluetooth handler to identify message update
+    public final static int CONNECTING_STATUS = 3; // used in bluetooth handler to identify message status
 
 
     @Override
@@ -33,17 +52,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LEDon = (Button) findViewById(R.id.ledON);
+        LEDoff = (Button) findViewById(R.id.ledOFF);
+        mBTStatus = (TextView) findViewById(R.id.btStatusView);
+        mReadBuffer = (TextView) findViewById(R.id.readBuffer);
+
+
+        LEDon.setClickable(true);
+        LEDoff.setClickable(true);
+        mBTStatus.setClickable(false);
+
+
         //checking if bluetooth exists on device
         if (btAdapter == null) {
             Toast.makeText(getApplicationContext(), "This device does not support Bluetooth", Toast.LENGTH_SHORT).show();
         } else {
-            if (!btAdapter.isEnabled())
-            { //if bt not enabled
+            if (!btAdapter.isEnabled()) { //if bt not enabled
                 Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableIntent, ENABLE_BT_REQUEST_CODE);
+                // flow goes to onActivityResult()
                 Toast.makeText(getApplicationContext(), "Enabling Bluetooth", Toast.LENGTH_SHORT).show();
             }
         }
+
+        mHandler = new Handler(Looper.getMainLooper()) {
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == MESSAGE_READ) {
+                    String readMessage = new String((byte[]) msg.obj, StandardCharsets.UTF_8);
+                    mReadBuffer.setText(readMessage);
+                }
+
+                if (msg.what == CONNECTING_STATUS) {
+                    if (msg.arg1 == 1) {
+                        btStatus = "Connected to Device: " + (String) (msg.obj);
+                        mBTStatus.setText(btStatus);
+                    } else {
+                        btStatus = "Connection Failed";
+                        mBTStatus.setText(btStatus);
+                    }
+
+                }
+            }
+        };
     }
 
     @Override
@@ -62,26 +112,17 @@ public class MainActivity extends AppCompatActivity {
                 else {
                     //boolean once = true;
                     if (pairedToEsk8()) {
-                        if (connectToEsk8()) {
-                            //--------------------------------------
-                            //
-                            // The specifics will vary depending on how you want your app to use
-                            // its newly-forged Bluetooth connection, but as a rough guideline,
-                            // you transfer data between two remote devices by completing the following steps:
-                            //
-                            // 1) Call getInputStream and getOutputStream on the BluetoothSocket.
-                            // 2) Use the read() method to start listening for incoming data.
-                            // 3) Send data to a remote device by calling the thread’s write() method and passing it the bytes you want to send.
-                            // 4)  Note that both the read() and write() methods are blocking calls, so you should always run them from a separate thread.
-                            //
-                            //--------------------------------------
-                        }
+                        //Connect as client to BT
+
+                        //Then
+                        //Go do ConnectedThread work
                     }
                 }
+
             }
 
             //If the request was unsuccessful...//
-            if(resultCode == RESULT_CANCELED){
+            if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(getApplicationContext(), "An error occurred while enabling Bluetooth",
                         Toast.LENGTH_SHORT).show();
             }
@@ -92,8 +133,7 @@ public class MainActivity extends AppCompatActivity {
         if (pairedDev.size() <= 0) {
             return false;
             //EXIT APPLICATION? TOAST- "Could not find e-Skateboard"
-        }
-        else {
+        } else {
             for (BluetoothDevice device : pairedDev) {
                 if (device.getName().equalsIgnoreCase("HC-06")) { //Change esk8 adapter name here
                     esk8 = device;
@@ -105,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
                         "Pair with HC-06 using device's Bluetooth settings",
                         Toast.LENGTH_LONG).show();
 
-                //Open BT settings for device if es8 HC-06 is not paired with device
+                //Open BT settings for device if esk8 HC-06 is not paired with device
                 Intent intentBluetooth = new Intent();
                 intentBluetooth.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
                 startActivity(intentBluetooth);
@@ -116,33 +156,89 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public boolean connectToEsk8() {
-        try {
-            BluetoothServerSocket bluetoothServerSocket = btAdapter.listenUsingRfcommWithServiceRecord("esk8", UUID.randomUUID());
-            btsocket = bluetoothServerSocket.accept(300);
-            bluetoothServerSocket.close();
-            if (btsocket.isConnected()) {
-                // btsocket.getInputStream() returns an inputstream
-                // btsocket.getOutputStream() returns an outputstream
+    public void sendOnMsg(View view) {
+        Toast.makeText(getApplicationContext(), "On LED ON clicked", Toast.LENGTH_SHORT).show();
+        //write to connected thread
 
-                // both must be run on separate threads to prevent blocking
-                // CAN ADD TEXT FIELD TO TELL APP STATE OF ESK8? LED ON OR OFF?
-                // Onclick button implementations to send specific data to arduino necessary
-                return true;
-            } else {
-                return false;
-            }
-        } catch (IOException ex) {
-            Throwable cause = ex.getCause();
-            Log.v(TAG, ex.getMessage());
-            ex.printStackTrace();
-            System.out.println("\n");
-            cause.printStackTrace();
-            return false;
-        }
     }
 
-    public void sendBTMsg(View view) {
+    public void sendOffMsg(View view) {
+        Toast.makeText(getApplicationContext(), "On LED OFF clicked", Toast.LENGTH_SHORT).show();
+        //write to connected thread
 
+    }
+
+
+    private BluetoothSocket createBluetoothSocket(@org.jetbrains.annotations.NotNull BluetoothDevice device) throws IOException {
+        return device.createRfcommSocketToServiceRecord(UUID.randomUUID());
+        //creates secure outgoing connection with BT device using a randomly generated UUID
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        private static final String TAG2 = "ConnectedThread";
+
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.available();
+                    if (bytes != 0) {
+                        SystemClock.sleep(100); //pause and wait for rest of data. Adjust this depending on your sending speed.
+                        bytes = mmInStream.available(); // how many bytes are ready to be read?
+                        bytes = mmInStream.read(buffer, 0, bytes); // record how many bytes we actually read
+                        mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                                .sendToTarget(); // Send the obtained bytes to the UI activity
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(String input) {
+            byte[] bytes = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
