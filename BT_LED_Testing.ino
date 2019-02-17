026e3235b333759852296799e0dd910a559b74d1
Aakash Ravichandran <33166705+pavlovedoncaffeine@users.noreply.github.com>
2019-02-16 21:52:08 -0500

    Tested and functioning BT/LED connection
    Arduino :
    RXpin = 4
    TXpin = 2
    LED digital write pin OUTPUT = 12
    
    HC-06:
    Rx header -> TXpin 2 on Uno
    Tx header -> RXpin 4 on Uno
    5V and grounded
    
    Biggest block in testing was the serial port allowing too many empty bytes through. Fixed by ensuring that read in data from BT to Uno is a digit before proceeding with if block. All other data is ignored. Next step, create state machine for multiple levels (10 step levels for power output from motor)

--- BT_LED_Testing.ino
+++ BT_LED_Testing.ino
@@ -1,26 +1,43 @@
-//Testing using code from a website. Have to test with his specified app.
-// Phone pairs with HC-06 but doesn't stay connected? Normal? 
-// Check if LED blinks as expected with app
+// Testing using code from a website. Have to test with his specified app.
+// Phone must pair with HC-06 and then Connect from app.
+// Check if LED blinks as expected with app?
 
-char data = 0;                //Variable for storing received data
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(4, 2);
+char led = 0;
 
-void setup()
-{
-  Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
-  pinMode(8, OUTPUT);        //Sets digital pin 8 as output pin
+void setup() {
+  //pinMode(3, INPUT);
+  //pinMode(2, OUTPUT);
+
+  Serial.begin(9600); //open the serial port
+  delay(200);
+  mySerial.begin(9600); // open the bluetooth serial port
+  delay(200);
+  pinMode(12, OUTPUT);
+  //delay(2000);
 }
+
 
 void loop()
 {
-  if (Serial.available() > 0) // Send data only when you receive data:
+  if (mySerial.available() > 0) // Send data only when you receive data:
   {
-    data = Serial.read();      //Read the incoming data and store it into variable data
-    Serial.print(data);        //Print Value inside data in Serial monitor
-    Serial.print("\n");        //New line
-    if (data == '1')           //Checks whether value of data is equal to 1
-      digitalWrite(8, HIGH);  //If value is 1 then LED turns ON
-    else if (data == '0')      //Checks whether value of data is equal to 0
-      digitalWrite(8, LOW);   //If value is 0 then LED turns OFF
+    char data = mySerial.read();
+    //led = isDigit(data) ? data : 'n';
+    if (isDigit(data)) {
+      Serial.println(data);
+      if ((int)data == 49)           //Checks whether value of data is equal to 1
+        digitalWrite(12, HIGH);  //If value is 1 then LED turns ON
+      else if ((int)data == 48)      //Checks whether value of data is equal to 0
+        digitalWrite(12, LOW);   //If value is 0 then LED turns OFF
+    } 
   }
 
+  if (Serial.available() > 0) {
+    mySerial.println(Serial.readString());  // send from bluetooth to serial
+  }
 }
+
+
+
