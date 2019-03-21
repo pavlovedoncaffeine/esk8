bf7c1c776bb0e997eb849ddc46db691def804408
Aakash Ravichandran <akash.ravichandran@gmail.com>
2019-03-21 18:31:08 -0400

    Updates to braking system arduino code
    Patch fix to previous code.
    
    Editted to correctly control power, center, braking and LED as expected.
    
    Removed only commented out code.

--- Arduino prototypes/Prototype_v1.0_esk8.ino
+++ Arduino prototypes/Prototype_v1.0_esk8.ino
@@ -6,10 +6,19 @@
 #include <Servo.h>
 
 #define BAUDRATE 9600
+
+// defines for bluetooth input states
 #define BT_ZERO 48
+#define BT_BRAKE 49
 #define BT_CENTER 50
+#define BT_POWER 51
 #define BT_MAX 53
+
+// defines for esc power states
 #define ESC_ZERO 90
+#define ESC_BRAKE 98
+#define ESC_CENTER 106
+#define ESC_POWER 114
 #define ESC_MAX 130
 
 #define LED_PIN 12
@@ -43,37 +52,40 @@ void loop()
   {
     data = mySerial.read(); //read data from bluetooth over serial
 
-    //    if (isDigit(data)) {
-    //      Serial.println(data);
-    //      if ((int)data == 49)           //Checks whether value of data is equal to 1
-    //        digitalWrite(12, HIGH);  //If value is 1 then LED turns ON
-    //      else if ((int)data == 48)      //Checks whether value of data is equal to 0
-    //        digitalWrite(12, LOW);   //If value is 0 then LED turns OFF
-    //    }
-    //  }
-
     if (isDigit(data)) {
       Serial.print("Data received from Bluetooth: ");
       Serial.println(data);
       mySerial.print("Data received from Bluetooth: ");
       mySerial.println(data);
-
+      
       val = map((int)data, BT_ZERO, BT_MAX, ESC_ZERO, ESC_MAX);
-      if ((int)data <= BT_CENTER) {
+      // Checking if input is coast or brake
+      if ((int)data <= BT_CENTER && (int)data >= BT_ZERO) {
         digitalWrite(LED_PIN, LOW);
-        val = ESC_ZERO;
-        ESC.write(ESC_ZERO);
-
-        Serial.println("BT_ZERO");
-      } else {
-        digitalWrite(LED_PIN, HIGH);
-        if ((int)data >= BT_MAX) {
-          data = BT_MAX;
-          val = ESC_MAX;
-          Serial.println("MAX");
+        // if mapped value to ESC is below what's possible, reset to ESC_ZERO (full brake)
+        if (val < ESC_ZERO) {
+          val = ESC_ZERO;
         }
         ESC.write(val);
-      }
+        Serial.println(data);
+      } // end if braking or coast
+
+      // Checking if input is power (3 levels)
+      else if ((int)data >= BT_POWER && (int)data <= BT_MAX) {
+        digitalWrite(LED_PIN, HIGH);
+        if (val > ESC_MAX) {
+          val = ESC_MAX;
+        }
+        ESC.write(val);
+      } // end else if power
+
+      // Checking if output is out of range (else)
+      else {
+        digitalWrite(LED_PIN, LOW);
+        val = ESC_ZERO;
+        ESC.write(val);
+      } // end else out of range
+      
     } // Comment out this section for troubleshooting
 
   }
